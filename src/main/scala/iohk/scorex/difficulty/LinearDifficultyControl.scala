bae1c43ade706c14121a6f48c71e309bd157d448
catena <catena@protonmail.com>
2017-07-31 12:39:56 +0300

    new diff

--- src/main/scala/iohk/scorex/difficulty/LinearDifficultyControl.scala
+++ src/main/scala/iohk/scorex/difficulty/LinearDifficultyControl.scala
@@ -6,22 +6,27 @@ import scala.concurrent.duration.FiniteDuration
 
 class LinearDifficultyControl(useLastEpochs: Int = 4) extends DifficultyControl {
 
-/*
-  override def diff(lastDiffs: Seq[(Difficulty, FiniteDuration)], desired: FiniteDuration): BigInt = {
-    if (lastDiffs.size >= 2) {
-      val realDiffs: Seq[BigInt] = lastDiffs.reverse.map(l => l._1 * desired.toMillis / l._2.toMillis)
-      val data: Seq[(Int, Difficulty)] = realDiffs.indices.map(i => i -> realDiffs(i)).takeRight(useLastEpochs)
 
-      val i = data.map(_._1).max + 1
-      val k = (2 * data.map(d => d._2 * d._1).sum + )
-      (2i−N −1)· n=i−N D n
+  override def diff(lastDiffs: Seq[(Difficulty, FiniteDuration)], desired: FiniteDuration): BigInt = {
+    if (lastDiffs.size >= useLastEpochs) {
+      val realDiffs: Seq[BigInt] = lastDiffs.reverse.map(l => l._1 * desired.toMillis / l._2.toMillis)
+      val D: Map[Int, BigDecimal] = realDiffs.indices.map(i => i -> BigDecimal(realDiffs(i))).takeRight(useLastEpochs).toMap
+
+      val i: Int = D.keys.max + 1
+      val N: Int = useLastEpochs
+      val xMean: BigDecimal = BigDecimal(2 * i - N - 1) / 2
+      val yMean = (((i - N) until i) map (n => D(n))).sum / N
+      val xyMean = (((i - N) until i) map (n => D(n) * n)).sum / N
+      val x2Mean = (((i - N) until i) map (n => BigDecimal(n) * n)).sum / N
+      val k: BigDecimal = (xyMean - xMean * yMean) / (x2Mean - xMean * xMean)
+      val b: BigDecimal = yMean - k * xMean
+      (k * i + b).toBigInt()
     } else lastDiffs.last._1
   }
-*/
 
 
-  def diff(lastDiffs: Seq[(Difficulty, FiniteDuration)], desired: FiniteDuration): BigInt = {
-    if (lastDiffs.size >= 2) {
+  def diffOld(lastDiffs: Seq[(Difficulty, FiniteDuration)], desired: FiniteDuration): BigInt = {
+    if (lastDiffs.size >= useLastEpochs) {
       val realDiffs: Seq[BigInt] = lastDiffs.reverse.map(l => l._1 * desired.toMillis / l._2.toMillis).toSeq
       val data: Seq[(Int, Difficulty)] = realDiffs.indices.map(i => i -> realDiffs(i)).takeRight(useLastEpochs)
       interpolate(data)(data.map(_._1).max + 1)
